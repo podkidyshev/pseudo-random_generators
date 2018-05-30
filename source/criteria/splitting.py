7f73b3fca64c0f589ea12e112d83efdd8e66a08d
Raashi <raashicat@gmail.com>
2018-05-30 11:51:16 +0400

    splitting criteria completed

--- source/criteria/splitting.py
+++ source/criteria/splitting.py
@@ -1,35 +1,30 @@
 from math import floor
-
+from sympy.functions.combinatorial.numbers import stirling
 import criteria.chi2 as chi2
 
 
 def splitting(prs_orig):
-    # Приводим к дискретному распределению от 0 до (число различных карт - 1)
-    prs = [floor(12 * v) for v in prs_orig]
+    # Данный критерий называется также покер-критерий
+    # Приводим к дискретному распределению от 0 до <число различных карт - 1=12>
+    d = 13  # число различных карт
+    prs = [floor(d * v) for v in prs_orig]
 
-    # будем рассматривать пятерки чисел
-    d = 12
-    k = 5
-    n = len(prs) - k
-    counts = [0] * k
+    k = 5              # пятерки чисел/карт
+    n = len(prs) // k  # число групп элементов
+    counts = [0] * k   # массив: в r-ой ячейке число групп с r+1-различными картами (нуль-нумерация)
 
     for i in range(n):
-        # число различных элементов
-        r = len(set(prs[i + j] for j in range(k)))
-        counts[r] += 1
+        # число различных элементов/карт в руке
+        r = len(set(prs[i * k + j] for j in range(k)))
+        counts[r - 1] += 1
 
-    """
-    k = 5
-    ni = [0] * k
-    values = {v: set() for v in range(k)}
+    plist = []
+    for r in range(1, k + 1):
+        pr = 1 / (d ** k)
+        st = stirling(k, r, kind=2)
+        pr *= st
+        for i in range(r):
+            pr *= d - i
+        plist.append(pr * n)
 
-    for v in prs:
-        res = min(floor(1000 * v) // k, k - 1)
-        ni[res] += 1
-        values[res].add(floor(1000 * v) % k)
-
-    stirling = [0, 720, 1764, 1624, 735, 175, 21, 1]
-    ej = [st * k for st in stirling]
-
-    chi2.chi2_conclusion(chi2.chi2_compute(ni, ej), chi2.chi2_stat(0.95, k))
-    """
+    chi2.chi2_conclusion(chi2.chi2_compute(counts, plist), chi2.chi2_stat(0.95, k))
